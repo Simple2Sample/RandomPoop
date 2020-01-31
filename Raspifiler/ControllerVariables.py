878ec4bbf88da2d7364a7410684781b229bd22cf
Simon <simon.saetre@gmail.com>
2020-01-31 12:37:50 +0100

    Programmet gjenkjenner kontrolleren og klarer å gi variabler verdiene fra kontrolleren

---
+++ Raspifiler/ControllerVariables.py
@@ -0,0 +1,63 @@
+
+#Range for joysticks are 16 bits or 65536 values. 32768 is center position.
+#Be aware of deadzones!!!
+#Code00
+#xAxis1
+code0 = 32768
+
+
+#Code01
+#yAxis1
+code1 = 32768
+
+#Code02
+#10 bit trigger. Ranging from 0 to 1023. 0 is unpressed.
+#leftTrigger
+code2 = 0
+
+#Code03
+#xAxis2
+code3 = 32768
+#Code04
+#yAxis2
+code4 = 32768
+
+#Code05
+#10 bit trigger. Ranging from 0 to 1023. 0 is unpressed
+#rightTrigger
+code5 = 0
+
+#Code304
+#AButton
+code304 = 0
+
+
+#Code305
+#BButton
+code305 = 0
+
+
+#Code306
+#XButton
+code306= 0
+
+
+#Code307
+#YButton
+code307 = 0
+
+#Select Button
+Code310 = 0
+#Start Button
+code311 = 0
+
+
+#Code16
+#Value ranges from -1 to 1. -1 means Left and 1 means right. 0 is no input
+#XCross
+code16 = 0
+
+#Code17
+#Value ranges from -1 to 1. -1 means up and 1 means down.
+#YCross
+code17 = 0
--- Raspifiler/FindController.py
+++ Raspifiler/FindController.py
@@ -1,8 +1,14 @@
 #import evdev
 from evdev import InputDevice, categorize, ecodes
-
+import os
+import ControllerVariables
+import AssignControllerValues
 #creates object 'gamepad' to store the data
 #you can call it whatever you like
+while not os.path.exists('/dev/input/event7'):
+    print('Waiting for connection')
+    
+
 gamepad = InputDevice('/dev/input/event7')
 
 #prints out device info at start
@@ -11,8 +17,7 @@ print(gamepad)
 #evdev takes care of polling the controller in a loop
 for event in gamepad.read_loop():
     #filters by event type
-    if event.type == ecodes.EV_KEY:
-        print(event)
-    else:
-        print(event)
-
+   # if event.type == ecodes.EV_KEY:
+    #    print(event)
+    
+    AssignControllerValues.assign_ValueToButtons(event)
